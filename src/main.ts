214e82dbc458d91b84e4b544158378ed76d7e8f0
eldanielhumberto <gamekik92@gmail.com>
2025-09-21 15:28:31 -0600

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import {BLOCK_SIZE, BOARD_HEIGHT, BOARD_WIDTH} from "./constants.ts";
 import {Figure} from "./Figure.ts";
 import {Board} from "./Board.ts";
 
+// Canvas config
 const canvas = document.getElementById("canvas") as HTMLCanvasElement
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
-
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
 
+// Init objects
 const board = new Board(ctx);
 const figure = new Figure({
     position: {x: 6, y: 0},
@@ -19,38 +20,38 @@ const figure = new Figure({
     ]
 }, ctx);
 
+// Controls
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowLeft") {
         figure.position.x--
-        if (figure.checkCollision()) {
-            figure.position.x++
-        }
+        if (figure.checkCollision()) figure.position.x++
     }
 
     if (event.key === "ArrowRight") {
         figure.position.x++
-        if (figure.checkCollision()) {
-            figure.position.x--
-        }
+        if (figure.checkCollision()) figure.position.x--
     }
 })
 
+// Draw objects
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-
     figure.draw()
     board.draw()
 }
 
+// Time control
+const DROP_INTERVAL = 200
 let dropCounter = 0
 let lastTime = 0
 
+// Loop
 function update(time = 0) {
     const deltaTime = time - lastTime
     lastTime = time
 
     dropCounter += deltaTime
-    if (dropCounter > 200) {
+    if (dropCounter > DROP_INTERVAL) {
         figure.position.y++
         dropCounter = 0
 
